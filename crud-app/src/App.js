5d21f3c74a1220ba3696d6af54fc21cdc743828e
Adhitya Primandhika <adhityaprimandhika@gmail.com>
2021-09-24 20:46:14 +0700

    add POST,GET,PUT,and DELETE function

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,7 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const [menuName, setMenuName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [newMenuName, setNewMenuName] = useState("");
+  const [newQuantity, setNewQuantity] = useState(0);
+  const [menuList, setMenuList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setMenuList(response.data);
+    });
+  }, []);
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      menuName: menuName,
+      price: price,
+      quantity: quantity,
+    });
+  };
+
+  const updateMenu = (id) => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      newMenuName: newMenuName,
+      newQuantity: newQuantity,
+    });
+  };
+
+  const deleteMenu = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const MenuModel = require("./models/Menu");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://newuser:password1234@crud.vplmy.mongodb.net/menu?retryWrites=true&w=majority",
@@ -13,8 +15,16 @@ mongoose.connect(
   }
 );
 
-app.get("/", async (req, res) => {
-  const menu = new MenuModel({ menuName: "Burger", price: 10000, quantity: 2 });
+app.post("/insert", async (req, res) => {
+  const menuName = req.body.menuName;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+
+  const menu = new MenuModel({
+    menuName: menuName,
+    price: price,
+    quantity: quantity,
+  });
 
   try {
     await menu.save();
@@ -22,6 +32,40 @@ app.get("/", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+app.get("/read", async (req, res) => {
+  MenuModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.send(result);
+  });
+});
+
+app.put("/update", async (req, res) => {
+  const newMenuName = req.body.newMenuName;
+  const newQuantity = req.body.newQuantity;
+  const id = req.body.id;
+
+  try {
+    MenuModel.findById(id, (err, updatedMenu) => {
+      updatedMenu.menuName = newMenuName;
+      updatedMenu.quantity = newQuantity;
+      updatedMenu.save();
+      res.send("update");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await MenuModel.findByIdAndRemove(id).exec();
+  res.send("deleted");
 });
 
 app.listen(3001, () => {
